224841f0035729ce9defec2849f67743c1ac9895
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2024-02-10 19:48:39 +1100

    update shasum

--- Casks/minion.rb
+++ Casks/minion.rb
@@ -1,6 +1,6 @@
 cask "minion" do
   version "1.0.7"
-  sha256 "d32ab213527534ca4224af66b6f3bec753546b5ba0d5894f12623bd63d34a835"
+  sha256 "cd2d475260fcab0aee50cd9f2a5b3be91dfefec6c384817fbda7b418485cc90c"
   url "https://github.com/3kwa/minion/releases/download/v#{version}/minion-darwin-arm64-#{version}.zip"
   name "minion"
   desc "a minimalistic HTML/CSS/JavaScript viewer"
