f8b4a6e86d400490a283a5c027741861efcd1002
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2024-06-29 22:01:51 +1000

    v1.1.0 with binary

--- Casks/minion.rb
+++ Casks/minion.rb
@@ -1,10 +1,11 @@
 cask "minion" do
-  version "1.0.9"
-  sha256 "b3b8f55f94e40e707e87051792767c831851d2dc942de0a05f21ed5e99e913d0"
+  version "1.1.0"
+  sha256 "33584a529356036edd6063a4bba08a92bfcd378af563a24997980e23fedd68fc"
   url "https://github.com/3kwa/minion/releases/download/v#{version}/minion-darwin-arm64-#{version}.zip"
   name "minion"
   desc "a minimalistic HTML/CSS/JavaScript viewer"
   homepage "https://github.com/3kwa/minion/"
 
   app "minion.app"
+  binary "#{appdir}/minion.app/Contents/MacOS/minion"
 end
