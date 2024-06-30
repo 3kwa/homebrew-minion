b12ad3223f8b57bd6ebb36784047c572e600901b
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2024-06-30 11:43:38 +1000

    stupid whitespace

--- Casks/minion.rb
+++ Casks/minion.rb
@@ -1,6 +1,6 @@
 cask "minion" do
   version "1.1.1"
-  sha256 "aa2756836fb0e3e709e38b8ea85697c4c6a19d319f707a09fdd21b9ae8e2366a "
+  sha256 "aa2756836fb0e3e709e38b8ea85697c4c6a19d319f707a09fdd21b9ae8e2366a"
   url "https://github.com/3kwa/minion/releases/download/v#{version}/minion-darwin-arm64-#{version}.zip"
   name "minion"
   desc "a minimalistic HTML/CSS/JavaScript viewer"
